b34788e554b9752891b85afbf007cc0be2dc5fa4
Renan Martins <renanrodm@gmail.com>
2021-11-19 18:47:35 -0300

    Solução do professor para o exercício

--- ex086.py
+++ ex086.py
@@ -1,20 +1,13 @@
 '''Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado.
 No final, mostre a matriz na tela, com a formatação correta.'''
 
-lin1 = [[], [], []]
-lin2 = [[], [], []]
-lin3 = [[], [], []]
-for i in range(0, 3):
-    valor = int(input(f"Digite um valor para [0, {i}]: "))
-    lin1[i].append(valor)
-for i in range(0, 3):
-    valor = int(input(f"Digite um valor para [1, {i}]: "))
-    lin2[i].append(valor)
-for i in range(0, 3):
-    valor = int(input(f"Digite um valor para [2, {i}]: "))
-    lin3[i].append(valor)
-print(f'{lin1}')
-print(f'{lin2}')
-print(f'{lin3}')
-
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f"Digite um valor para a posição [{l},{c}] "))
+print("-="*30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
 
