1c72f8b69fa91e7f529726206a5ab5ef45638ec1
Renan Martins <renanrodm@gmail.com>
2021-11-19 19:25:56 -0300

    Segunda versão do exercício

--- ex087.py
+++ ex087.py
@@ -4,30 +4,30 @@ B) A soma dos valroes da terceira coluna.
 C) O maior valor  da segunda linha'''
 
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+soma_par = 0
+soma_col = 0
+maior_valor = matriz[1][0]
+#loop de alimentação da matriz
 for l in range(0, 3):
     for c in range(0, 3):
-        matriz[l][c] = int(input(f"Digite um valor para a posição [{l},{c}] "))
+        matriz[l][c] = int(input(f"Digite um valor para a posição [{l},{c}]: "))
+
 print("-="*30)
+#loop de exibição e operações na matriz
 for l in range(0, 3):
+    # somando os valores da terceira coluna
+    soma_col += matriz[l][2]
     for c in range(0, 3):
+        #somando os valores pares
+        if matriz[l][c] % 2 == 0:
+            soma_par += matriz[l][c]
+        #maior valor da segunda coluna
+        if matriz[1][c] > maior_valor:
+            maior_valor = matriz[1][c]
+        #exibindo matriz na tela de forma formata
         print(f'[{matriz[l][c]:^5}]', end='')
     print()
 print("-="*30)
-#soma dos valores pares
-soma_par = 0
-for l in range(0, 3):
-    for c in range(0, 3):
-        if matriz[l][c] % 2 == 0:
-            soma_par += matriz[l][c]
-#soma dos valores da terceira coluna
-soma_terc = 0
-for l in range(0, 3):
-    soma_terc += matriz[l][2]
-#maior valor da segunda linha
-maior_valor = matriz[1][0]
-for c in range(0, 3):
-    if matriz[1][c] > maior_valor:
-        maior_valor = matriz[1][c]
 print(f'A soma dos valores pares é {soma_par}')
-print(f'A soma dos valores da terceira coluna é {soma_terc}')
+print(f'A soma dos valores da terceira coluna é {soma_col}')
 print(f'O maior valor da segunda linha é {maior_valor}')
