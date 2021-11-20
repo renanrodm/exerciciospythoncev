caf2cf941966c1ce6897d0cdacc9b324690c87bf
Renan Martins <renanrodm@gmail.com>
2021-11-20 20:21:04 -0300

    versão final 2.0 do exercício

--- ex088.py
+++ ex088.py
@@ -1,18 +1,16 @@
 '''Faça um programa que ajude um jogador da MEGA SENA a criar palpites. O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.'''
-import random
-import time
+from random import randint
 qtde_jogos = int(input("Quantos jogos você quer que eu sorteie? -> "))
 lista = []
 for c in range(0, qtde_jogos):
     cont = 0
     while True:
-        numero = random.randint(1, 60)
-        if cont == 0:
-            lista.append(numero)
+        numero = randint(1, 60)
         if numero not in lista:
             lista.append(numero)
-        cont += 1
-        if len(lista) == 6:
+            cont += 1
+        if cont == 6:
             break
+    lista.sort()
     print(f'Jogo {c+1}: {lista}')
     lista.clear()
