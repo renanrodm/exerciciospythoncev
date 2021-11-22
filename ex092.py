dc0589e4b2311d16f98d3c2cafb638ddf341c7ed
Renan Martins <renanrodm@gmail.com>
2021-11-22 16:54:33 -0300

    adicionado o módulo datetime para puxar o ano atual para calculo de idade e aposentadoria

--- ex092.py
+++ ex092.py
@@ -1,19 +1,19 @@
 '''Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano da contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
 
 Considerando aposentadoria dps de 35 anos de contribuição.'''
-
+from datetime import datetime
 dados = dict()
 # estrutura de alimentação do dicionário
 
 dados['nome'] = str(input('Nome: '))
 nascimento = int(input('Ano de Nascimento: '))
-dados['idade'] = int(2018 - nascimento)
+dados['idade'] = datetime.now().year - nascimento
 dados['ctps'] = int(input('Carteira de trabalho (0 se não tem): '))
 
 if dados['ctps'] != 0:
     dados['contratação'] = int(input('Ano da contratação: '))
     dados['salario'] = float(input('Salário: R$ '))
-    dados['aposentadoria'] = (35 - (2018 - dados['contratação'])) + dados['idade']
+    dados['aposentadoria'] = (35 - (datetime.now().year - dados['contratação'])) + dados['idade']
 else:
     del dados['ctps']
 print('-='*30)
