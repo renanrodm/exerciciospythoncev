ece43b2ebf800810fcd79cfb08aa8632d64f3441
Renan Martins <renanrodm@gmail.com>
2021-11-22 20:20:26 -0300

    adicionado estruturas para exibição dos itens A, B e C conforme solicitado.

--- ex094.py
+++ ex094.py
@@ -5,15 +5,21 @@ C) Uma lista com todas as mulheres.
 D) Uma lista com todas as pessoas com idade acima da média'''
 pessoas = list()
 individuos = dict()
+mulheres = list()
+idade_maior_media = list()
+tot_pessoas = 0
+soma_idade = 0
 while True:
     # estrutura para armazenamento dos dados
     individuos['nome'] = str(input('Nome: '))
-    individuos['sexo'] = str(input('Sexo [M/F]: '))
+    individuos['sexo'] = str(input('Sexo [M/F]: ')).upper()
     while individuos['sexo'] not in 'MmFf': # tratamento de erro de string
         print('ERRO! Por favor, digite apenas M ou F.')
-        individuos['sexo'] = str(input('Sexo [M/F]: '))
+        individuos['sexo'] = str(input('Sexo [M/F]: ')).upper()
     individuos['idade'] = int(input('Idade: '))
+    soma_idade += individuos['idade'] #acumula idade para cálculo da média
     pessoas.append(individuos.copy())
+    tot_pessoas += 1 #conta quantas pessoas foram adicionadas
     print('-='*30)
     # estrutura de confirmação para continuação do programa
     opc = str(input('Quer continuar? [S/N]: '))
@@ -22,3 +28,21 @@ while True:
         opc = str(input('Quer continuar? [S/N]: '))
     if opc in 'Nn':
         break
+print('-='*30)
+print(pessoas)
+#estrutura de exibição básica dos itens A e B
+print(f'A) Ao todo temos {tot_pessoas} pessoas cadastradas.')
+media_idade = soma_idade/tot_pessoas
+print(f'B) A média de idade é de {media_idade:.2f} anos.')
+
+# estrutura de exibição do item C
+for i, v in enumerate(pessoas):
+
+    if pessoas[i]['sexo'] == 'F':# adicionando pessoas do sexo feminino para uma lista
+        mulheres.append(v['nome'])
+
+    if pessoas[i]['idade'] > media_idade: # adicionado pessoas acima da media de idade para uma lista
+        idade_maior_media.append(v.copy())
+print(f'C) As mulheres cadastradas foram {mulheres}')
+
+# estrutura para exibir pessoas com idade acima da média
