edf4b2270fc1d04ea236e8de8648af49e781d067
Renan Martins <renanrodm@gmail.com>
2021-11-22 16:30:17 -0300

    primeira versão da minha solução para o ex 092

---
+++ ex092.py
@@ -0,0 +1,20 @@
+'''Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano da contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+
+Considerando aposentadoria dps de 35 anos de contribuição.'''
+
+dados = dict()
+# estrutura de alimentação do dicionário
+dados['nome'] = str(input('Nome: '))
+nascimento = int(input('Ano de Nascimento: '))
+dados['idade'] = int(2018 - nascimento)
+dados['ctps'] = int(input('Carteira de trabalho (0 se não tem): '))
+if dados['ctps'] != 0:
+    dados['contratação'] = int(input('Ano da contratação: '))
+    dados['salario'] = float(input('Salário: R$ '))
+    dados['aposentadoria'] = 35 - (2018 - dados['contratação'])
+else:
+    del dados['ctps']
+print('-='*30)
+# estrutura de exibição dos itens
+for k, v in dados.items():
+    print(f'- {k} tem o valor {v} ')
