f3f5be81c60fb9bbaeb2f243a409488b8e87637a
Renan Martins <renanrodm@gmail.com>
2021-11-22 09:06:08 -0300

    adicionado linhas de comentários

--- ex090.py
+++ ex090.py
@@ -1,7 +1,7 @@
 '''Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.'''
 
 aluno_dados = dict()
-
+#estrutura de alimentação de dados
 aluno_dados['nome'] = str(input('Nome: '))
 aluno_dados['media'] = float(input('Media: '))
 if aluno_dados['media'] >= 7:
@@ -10,7 +10,7 @@ elif 5 <= aluno_dados['media'] < 7:
     aluno_dados['situação'] = 'RECUPERAÇÃO'
 elif aluno_dados['media'] < 5:
     aluno_dados['situação'] = 'REPROVADO!'
-
+# estrutura de exibição dos dados do dicionário
 print(f'- nome é igual a {aluno_dados["nome"]}')
 print(f'- média é igual a {aluno_dados["media"]}')
 print(f'- situação é igual {aluno_dados["situação"]}')
