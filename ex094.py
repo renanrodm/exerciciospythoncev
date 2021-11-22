67f1b363794398d07ac235bdbb59de25b3d7dfff
Renan Martins <renanrodm@gmail.com>
2021-11-22 19:04:44 -0300

    ex094: versão inicial da minha solução do exercício

--- ex094.py
+++ ex094.py
@@ -3,3 +3,21 @@ A) Quantas pessoas foram cadastradas
 B) A média de idade do grupo
 C) Uma lista com todas as mulheres.
 D) Uma lista com todas as pessoas com idade acima da média'''
+pessoas = list()
+individuos = dict()
+while True:
+    # estrutura para armazenamento dos dados
+    individuos['nome'] = str(input('Nome: '))
+    individuos['sexo'] = str(input('Sexo [M/F]: '))
+    while individuos['sexo'] not in 'MmFf': # tratamento de erro de string
+        print('ERRO! Por favor, digite apenas M ou F.')
+        individuos['sexo'] = str(input('Sexo [M/F]: '))
+    individuos['idade'] = int(input('Idade: '))
+
+    # estrutura de confirmação para continuação do programa
+    opc = str(input('Quer continuar? [S/N]: '))
+    while opc not in 'SsNn':
+        print('ERRO! Por favor, digite apenas S ou N.')
+        opc = str(input('Quer continuar? [S/N]: '))
+    if opc in 'Nn':
+        break
