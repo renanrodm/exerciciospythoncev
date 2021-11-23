c6e81e558066dc42d84192ed8e1dc37c4803d9b5
Renan Martins <renanrodm@gmail.com>
2021-11-22 23:39:53 -0300

    ex094: adicionado poucas melhorias no código, sem alteração da lógica

--- ex094.py
+++ ex094.py
@@ -9,20 +9,24 @@ individuos = dict()
 mulheres = list()
 idade_maior_media = list()
 tot_pessoas = 0
-soma_idade = 0
+tot_idade = 0
 
 while True:
     # estrutura para armazenamento dos dados
     individuos['nome'] = str(input('Nome: '))
+
     individuos['sexo'] = str(input('Sexo [M/F]: ')).upper()
-    while individuos['sexo'] not in 'MmFf': # tratamento de erro de string
+    while individuos['sexo'] not in 'MmFf':
         print('ERRO! Por favor, digite apenas M ou F.')
         individuos['sexo'] = str(input('Sexo [M/F]: ')).upper()
+
     individuos['idade'] = int(input('Idade: '))
-    soma_idade += individuos['idade'] #acumula idade para cálculo da média
+    tot_idade += individuos['idade']
+
     pessoas.append(individuos.copy())
-    tot_pessoas += 1 #conta quantas pessoas foram adicionadas
-    print('-='*30)
+
+    print('-=' * 30)
+
     # estrutura de confirmação para continuação do programa
     opc = str(input('Quer continuar? [S/N]: '))
     while opc not in 'SsNn':
@@ -30,26 +34,29 @@ while True:
         opc = str(input('Quer continuar? [S/N]: '))
     if opc in 'Nn':
         break
-print('-='*30)
+print('-=' * 30)
 
-#estrutura de exibição básica dos itens A e B
-print(f'A) Ao todo temos {tot_pessoas} pessoas cadastradas.')
-media_idade = soma_idade/tot_pessoas
+# estrutura de exibição básica dos itens A e B
+print(f'A) Ao todo temos {len(pessoas)} pessoas cadastradas.')
+
+media_idade = tot_idade / len(pessoas)
 print(f'B) A média de idade é de {media_idade:.2f} anos.')
 
-# estrutura de exibição do item C
+# estrutura de criação da lista de mulheres e idades acima da média
 for i, v in enumerate(pessoas):
 
-    if pessoas[i]['sexo'] == 'F':# adicionando pessoas do sexo feminino para uma lista
+    if pessoas[i]['sexo'] == 'F':  # adicionando pessoas do sexo feminino para uma lista
         mulheres.append(v['nome'])
 
-    if pessoas[i]['idade'] > media_idade: # adicionado pessoas acima da media de idade para uma lista
+    if pessoas[i]['idade'] > media_idade:  # adicionado pessoas acima da media de idade para uma lista
         idade_maior_media.append(v.copy())
+
 # estrutura para exibir a lista de mulheres cadastradas
 print(f'C) As mulheres cadastradas foram {mulheres}')
 
 # estrutura para exibir pessoas com idade acima da média
 print('D) Lista das pessoas que estão acima da média: ')
 for i, v in enumerate(idade_maior_media):
-    print(f'    nome = {idade_maior_media[i]["nome"]}; sexo = {idade_maior_media[i]["sexo"]}; idade = {idade_maior_media[i]["idade"]}')
+    print(
+        f'    nome = {idade_maior_media[i]["nome"]}; sexo = {idade_maior_media[i]["sexo"]}; idade = {idade_maior_media[i]["idade"]}')
 print('<< ENCERRADO >>')
