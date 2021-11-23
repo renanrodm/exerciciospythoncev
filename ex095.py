af83b40ced5d9c3aad516423a0ca183e506b9e0f
Renan Martins <renanrodm@gmail.com>
2021-11-23 19:30:24 -0300

    ex095: adicionado versão inicial da estrutura de armazenamento de dados

--- ex095.py
+++ ex095.py
@@ -1,15 +1,24 @@
 '''Aprimore o desafio 093 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamente de cada jogador.'''
 
-jogador = dict()
-jogadores = list()
-gols = list()
+jogadores = dict()
+time = list()
+
+# estrutura de armazenamento dos dados
 
 while True:
-    gols.clear()
-    jogador['nome'] = str(input("Nome do jogador: "))
-    partidas = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    jogadores['nome'] = str(input("Nome do jogador: "))
+    partidas = int(input(f'Quantas partidas {jogadores["nome"]} jogou? '))
 
+    jogadores['gols'] = list()
     for c in range(0, partidas):
-        gols.append(int(input(f'     Quantos gols na partida {c+1}? ')))
-    jogador['gols'] = gols.copy()
-    print(jogador)
+        jogadores['gols'].append(int(input(f'     Quantos gols na partida {c+1}? ')))
+
+    time.append(jogadores.copy())
+
+    while True:
+        resp = input("Quer continuar? [S/N] ").upper()[0]
+        if resp in "SN":
+            break
+    if resp in "N":
+        break
+print(time)
