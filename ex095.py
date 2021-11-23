8b68908015df2a88feeba334567e86d9252aca76
Renan Martins <renanrodm@gmail.com>
2021-11-23 20:37:39 -0300

    ex095: adicionado estrutura de exibição

--- ex095.py
+++ ex095.py
@@ -2,17 +2,19 @@
 
 jogadores = dict()
 time = list()
-
 # estrutura de armazenamento dos dados
 
 while True:
+    tot_gols = 0
     jogadores['nome'] = str(input("Nome do jogador: "))
     partidas = int(input(f'Quantas partidas {jogadores["nome"]} jogou? '))
 
     jogadores['gols'] = list()
     for c in range(0, partidas):
-        jogadores['gols'].append(int(input(f'     Quantos gols na partida {c+1}? ')))
-
+        gols = int(input(f'     Quantos gols na partida {c+1}? '))
+        tot_gols += gols
+        jogadores['gols'].append(gols)
+    jogadores['total'] = tot_gols
     time.append(jogadores.copy())
 
     while True:
@@ -21,4 +23,11 @@ while True:
             break
     if resp in "N":
         break
-print(time)
+print("-*"*30)
+
+# estrutura de exibição
+
+print("-"*30)
+for i, j in enumerate(time):
+    print(f'{i:<2} {j["nome"]:<15} {j["gols"]} {j["total"]:<5}')
+print("-"*30)
