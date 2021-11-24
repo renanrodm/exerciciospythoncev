56e9574124353a15d6a997b7c2cfd423cf737862
Renan Martins <renanrodm@gmail.com>
2021-11-24 15:35:24 -0300

    ex093: versão final da minha solução com melhorias geral no código

--- ex093.py
+++ ex093.py
@@ -1,21 +1,30 @@
 '''Crie um programa que gerencie o aproveitamente de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.'''
 
-dados = {'nome': str(input('Nome do jogador: ')), 'gols': list()}
-total = 0
-partidas = int(input(f'Quantas partidas {dados["nome"]} jogou? '))
-for c in range(0, partidas):
-    gols_partida = int(input(f'     Quantos gols na partida {c}: '))
-    dados['gols'].append(gols_partida)
-    total += gols_partida
-dados['total'] = total
+jogador = dict()
+partidas = list()
+
+jogador['nome'] = str(input('Nome do jogador: '))
+tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+
+for c in range(0, tot):
+    partidas.append(int(input(f'    Quantos gols na partida {c}: ')))
+jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
 
 print('-='*30)
-print(dados)
+
+print(jogador)
+
 print('-='*30)
-for k, v in dados.items():
-    print(f'O campo {k} tem valor {v}')
-print('-='*30)
-print(f'O jogador {dados["nome"]} jogou {partidas} partidas.')
-for i, v in enumerate(dados['gols']):
-    print(f'    => Na partida {i}, fez {v} gols.')
-print(f'Foi um total de {dados["total"]} gols.')
+
+for k, v in jogador.items():
+    print(f'O campo {k} tem valor {v}.')
+
+print('-=' * 30)
+
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
+
+for i, v in enumerate(jogador['gols']):
+    print(f'    => Na partida {i}, fez {jogador["gols"][i]} gols.')
+
+print(f'Foi um total de {jogador["total"]} gols.')
