b1752fe1261879847bc6fbec1713b5d5cf95fd86
Renan Martins <renanrodm@gmail.com>
2021-11-25 17:00:45 -0300

    ex098: versão final corrigida

--- ex098.py
+++ ex098.py
@@ -8,34 +8,34 @@ c) Uma contagem personalizada.'''
 from time import sleep
 
 
-def contador(inicio, fim, passo):
+def contador(i, f, p):
+    if p == 0:
+        p = 1
+    if p < 0:
+        p = p * (-1)
+    print("-=" * 30)
+    print(f'Contando de {i} até {f} de {p} em {p}:')
 
-    if passo < 0:
-        passo = passo * (-1)
-
-    print(f'Contando de {inicio} até {fim} de {passo} em {passo}:')
-    if inicio < fim:
-
-        cont = inicio
-        while cont <= fim:
+    if i < f:
+        cont = i
+        while cont <= f:
             print(f'{cont} ', end='')
-            cont += passo
+            sleep(0.4)
+            cont += p
+        print(f'-> FIM')
+    elif i > f:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='')
+            sleep(0.4)
+            cont -= p
         print(f'-> FIM')
 
-    elif inicio > fim:
-        cont = inicio
-        while cont >= fim:
-            print(f'{cont} ', end='')
-            cont = cont - passo
-        print(f'-> FIM')
-        print("-=" * 30)
 
-
-print("-=" * 30)
 contador(1, 10, 1)
 contador(10, 0, 2)
-print(f'Agora é sua vez de personalizar a contagem')
-i = int(input('Inicio: '))
-f = int(input('Fim: '))
-p = int(input('Passo: '))
-contador(i, f, p)
+print(f'Agora é sua vez de personalizar a contagem!')
+inicio = int(input('Inicio: '))
+fim = int(input('Fim: '))
+passo = int(input('Passo: '))
+contador(inicio, fim, passo)
