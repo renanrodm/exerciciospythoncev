b8f5bd24cb4d5c554140284c161d3920c197ab43
Renan Martins <renanrodm@gmail.com>
2021-11-25 20:09:46 -0300

    ex100: adicionado sleep

--- ex100.py
+++ ex100.py
@@ -1,13 +1,15 @@
 '''Faça um programa que tenha uma lista chamda números e duas funções chamadas sorteia() e somaPar(). A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores PARES sorteados pela função anterior.'''
 from random import randint
-
+from time import sleep
 
 def sorteia(lst):
     print(f'Sorteando 5 valores da lista:', end=' ')
     for c in range(0, 5):
         num = randint(1, 10)
         print(f'{num}', end=' ')
+        sleep(0.35)
         lst.append(num)
+    sleep(0.2)
     print(f'PRONTO!')
 
 
