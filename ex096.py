cc3e8daa6ff9b2004785595d11b3243cabae0c0c
Renan Martins <renanrodm@gmail.com>
2021-11-24 22:14:27 -0300

    ex096: versão final da minha solução

--- ex096.py
+++ ex096.py
@@ -1,1 +1,15 @@
 '''Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.'''
+
+
+def area(larg, comp):
+    area = larg * comp
+    print(f'A area de um terreno {larg:.1f}x{comp:.1f} é de {area}m².')
+
+
+print('-' * 30)
+print(f'{"Controle de Terreno":^30}')
+print('-'*30)
+
+largura = float(input('Largura (m): '))
+comprimento = float(input('Comprimento (m): '))
+area(largura, comprimento)
