f8d6e689b4a07e321b3d00f43806c5eb11435852
Renan Martins <renanrodm@gmail.com>
2021-11-25 18:45:11 -0300

    primeira versão final da minha solução para o ex099

--- ex099.py
+++ ex099.py
@@ -1,2 +1,22 @@
 '''Faça um programa que tenha um função chamada maior(), que receba vários parâmetros com valores inteiros.
 Seu programa tem que analisar todos os valores e dizer qual deles é maior'''
+def maior(*nums):
+    print('-='*30)
+    print('Analisando os valores passados...')
+    if len(nums) == 0:
+        print(f'Não foram informados valores.')
+    else:
+        mai = 0
+        for c in nums:
+            print(c, end=' ')
+            if c > mai:
+                mai = c
+        print(f'Foram informados {len(nums)} valores ao todo.')
+        print(f'O maior valor informado foi {mai}')
+
+
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
