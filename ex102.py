c752f8186ffe8f5f14f1d2cc189e55368a05fa4a
Renan Martins <renanrodm@gmail.com>
2021-11-26 17:05:48 -0300

    ex102: adicionado docstring ao exercício

--- ex102.py
+++ ex102.py
@@ -1,17 +1,25 @@
 '''Crie um programa que tenha uma função fatorial() que receba dois parâmetros:
 O primeiro que indique o número a calcular e o outro chamado show, que será um valor lógico (opcional) indicando se será mostrando ou não na tela o processo de cálculo do fatorial.'''
 
+
 def fatorial(num, show=False):
+    """
+    -> Calcula o Fatorial de um número.
+    :param num: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor do fatorial de um número n.
+    """
     if show:
         print(f'{num}', end=' ')
-        for f in range(num-1, 0, -1):
+        for f in range(num - 1, 0, -1):
             print(f'x {f}', end=' ')
             num *= f
         print(f'= {num}')
     else:
-        for f in range(num-1, 0, -1):
+        for f in range(num - 1, 0, -1):
             num *= f
-        return num
+        return print(num)
 
 
-fatorial(5, show=True)
+fatorial(7, show=True)
+# help(fatorial)
