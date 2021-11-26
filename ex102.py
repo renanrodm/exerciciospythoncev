171e41e9cb20af2fa63e4ae8ffee8d34eaa67d37
Renan Martins <renanrodm@gmail.com>
2021-11-26 17:00:33 -0300

    ex102: primeira versão da minha solução

--- ex102.py
+++ ex102.py
@@ -1,2 +1,17 @@
 '''Crie um programa que tenha uma função fatorial() que receba dois parâmetros:
 O primeiro que indique o número a calcular e o outro chamado show, que será um valor lógico (opcional) indicando se será mostrando ou não na tela o processo de cálculo do fatorial.'''
+
+def fatorial(num, show=False):
+    if show:
+        print(f'{num}', end=' ')
+        for f in range(num-1, 0, -1):
+            print(f'x {f}', end=' ')
+            num *= f
+        print(f'= {num}')
+    else:
+        for f in range(num-1, 0, -1):
+            num *= f
+        return num
+
+
+fatorial(5, show=True)
