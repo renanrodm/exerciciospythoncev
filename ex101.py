29c08217bc206ed5facefda49e9afe6dc5e0f63c
Renan Martins <renanrodm@gmail.com>
2021-11-26 16:30:09 -0300

    ex101: atualizada as condições da função

--- ex101.py
+++ ex101.py
@@ -1,17 +1,17 @@
 '''Crie um programaque tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.'''
 
 
-
 def voto(i):
     from datetime import date
     idade = date.today().year - i
 
     if idade <= 15:
         return f"Com {idade} anos de idade: NÃO VOTA."
-    elif 16 <= idade <= 17:
+    elif 16 <= idade < 18 or idade > 65:
         return f"Com {idade} anos de idade: VOTO OPCIONAL."
-    elif idade >= 18:
+    else:
         return f"Com {idade} anos de idade: VOTO OBRIGATÓRIO."
+
 
 print('-'*30)
 ano = int(input('Quem que ano você nasceu? '))
