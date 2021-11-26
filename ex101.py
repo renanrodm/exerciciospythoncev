f82738d0e2d5e97b7354f41fbda9ee2229539ad8
Renan Martins <renanrodm@gmail.com>
2021-11-26 00:20:47 -0300

    initial commit

---
+++ ex101.py
@@ -0,0 +1,1 @@
+'''Crie um programaque tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.'''
---
+++ ex102.py
@@ -0,0 +1,2 @@
+'''Crie um programa que tenha uma função fatorial() que receba dois parâmetros:
+O primeiro que indique o número a calcular e o outro chamado show, que será um valor lógico (opcional) indicando se será mostrando ou não na tela o processo de cálculo do fatorial.'''
