bd18de45a78da9a4f295164ba0f287f9c5907efb
Renan Martins <renanrodm@gmail.com>
2021-11-28 20:56:29 -0300

    ex108: minha solução

--- ex107/programa principal.py
+++ ex107/programa principal.py
@@ -1,7 +1,7 @@
 from ex107 import moeda
 
 p = float(input('Digite o preço: R$ '))
-print(f'A metade de R$ {p} é R$ {moeda.metade(p)}')
-print(f'O drobro de R$ {p} é R$ {moeda.dobro(p)}')
-print(f'Aumentando 10%, temos R$ {moeda.aumentar(p, 10):.2f}')
-print(f'Reduzindo 13%, temos R$ {moeda.diminuir(p, 13):.2f}')
+print(f'A metade de {p} é {moeda.metade(p)}')
+print(f'O drobro de {p} é {moeda.dobro(p)}')
+print(f'Aumentando 10%, temos {moeda.aumentar(p, 10)}')
+print(f'Reduzindo 13%, temos {moeda.diminuir(p, 13)}')
