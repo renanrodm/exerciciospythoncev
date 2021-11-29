9c0ca742bddcb67e904255db2f8ab9efb47e009b
Renan Martins <renanrodm@gmail.com>
2021-11-29 00:39:53 -0300

    melhora na legibilidade das funções

--- ex110/moeda.py
+++ ex110/moeda.py
@@ -1,30 +1,46 @@
 
-def resumo(preço=0, aumento=0, redução=0):
+def resumo(preço=0, aumento=10, redução=5):
     print('-'*30)
-    print(f'{"RESUMO DO VALOR":^30}')
+    print(f'{"RESUMO DO VALOR".center(30)}')
     print('-'*30)
     print(f'{"Preço analisado: ":<20}', f'{moeda(preço):<}')
-    print(f'{"Dobro do preço:":<20}', f'{moeda(dobro(preço)):<}')
-    print(f'{"Metade do preço:":<20}', f'{moeda(metade(preço)):<}')
-    print(f'{f"{aumento}% de aumento:":<20}', f'{moeda(aumentar(preço, aumento)):<}')
-    print(f'{f"{redução}% de redução:":<20}', f'{moeda(diminuir(preço, redução)):<}')
+    print(f'{"Dobro do preço:":<20}', f'{dobro(preço, True):<}')
+    print(f'{"Metade do preço:":<20}', f'{metade(preço, True):<}')
+    print(f'{f"{aumento}% de aumento:":<20}', f'{aumentar(preço, aumento, True):<}')
+    print(f'{f"{redução}% de redução:":<20}', f'{diminuir(preço, redução, True):<}')
     print('-' * 30)
 
 
-def metade(preço=0):
-    return preço / 2
+def metade(preço=0, formatado=False):
+    res = preço / 2
+    if formatado:
+        return moeda(res)
+    else:
+        return res
 
 
-def dobro(preço=0):
-    return preço * 2
+def dobro(preço=0, formatado=False):
+    res = preço * 2
+    if formatado:
+        return moeda(res)
+    else:
+        return res
 
 
-def aumentar(preço=0, taxa=0):
-    return preço * (1 + (taxa / 100))
+def aumentar(preço=0, taxa=0, formatado=False):
+    res = preço * (1 + (taxa / 100))
+    if formatado:
+        return moeda(res)
+    else:
+        return res
 
 
-def diminuir(preço=0, taxa=0):
-    return preço * (1 - (taxa / 100))
+def diminuir(preço=0, taxa=0, formatado=False):
+    res = preço * (1 - (taxa / 100))
+    if formatado:
+        return moeda(res)
+    else:
+        return res
 
 
 def moeda(preço=0.0, moeda="R$"):
