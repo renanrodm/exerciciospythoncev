1fdc1b932eb69b4f47a361d8d719c4617b0c74ec
Renan Martins <renanrodm@gmail.com>
2021-11-28 22:53:52 -0300

    ex109: minha solução final para o exercício

--- ex109/moeda.py
+++ ex109/moeda.py
@@ -1,22 +1,30 @@
-def metade(preço=0):
+def metade(preço=0, formatação=False):
     res = preço / 2
+    if formatação:
+        res = moeda(res)
     return res
 
 
-def dobro(preço=0):
+def dobro(preço=0, formatação=False):
     res = preço * 2
+    if formatação:
+        res = moeda(res)
     return res
 
 
-def aumentar(preço=0, taxa=0):
-    res = preço * (1 + (taxa/100))
+def aumentar(preço=0, taxa=0, formatação=False):
+    res = preço * (1 + (taxa / 100))
+    if formatação:
+        res = moeda(res)
     return res
 
 
-def diminuir(preço = 0, taxa=0):
-    res = preço * (1 - (taxa/100))
+def diminuir(preço=0, taxa=0, formatação=False):
+    res = preço * (1 - (taxa / 100))
+    if formatação:
+        res = moeda(res)
     return res
 
 
-def moeda(preço = 0, moeda="R$"):
+def moeda(preço=0, moeda="R$"):
     return f'{moeda}{preço:.2f}'.replace(".", ",")
