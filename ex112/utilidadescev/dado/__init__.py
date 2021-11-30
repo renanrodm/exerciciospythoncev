4af803459a4ce08ba2adcad309c2cf50bc7cd88c
Renan Martins <renanrodm@gmail.com>
2021-11-29 23:42:21 -0300

    melhora o sistema de validação de dados, agora reconhecendo valores moentários com ponto ou vírgula.

--- ex112/utilidadescev/dado/__init__.py
+++ ex112/utilidadescev/dado/__init__.py
@@ -1,7 +1,16 @@
 def leiaDinheiro(msg):
     while True:
         num = input(msg)
-        if num.isnumeric():
-            return float(num)
-        print(f'\033[1;31mERRO! {num} é um preço inválido!\033[0:0m')
+        num2 = num
+
+        if "," in num:
+            num = num.split(",")
+        elif "." in num:
+            num = num.split(".")
+        #verifica se é número
+        if len(num) == 2:
+            if num[0].isnumeric() and num[1].isnumeric():
+                num = num[0] + "." + num[1]
+                return float(num)
+        print(f'\033[1;31mERRO! "{num2}" é um preço inválido!\033[0:0m')
 
