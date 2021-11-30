09578c93b531207e48c9cd4900666d1fd626c75b
Renan Martins <renanrodm@gmail.com>
2021-11-30 17:02:33 -0300

    ex112: modificado a validação de dados de acordo com solução do professor

--- ex112/utilidadescev/dado/__init__.py
+++ ex112/utilidadescev/dado/__init__.py
@@ -1,21 +1,9 @@
 def leiaDinheiro(msg):
-    while True:
-        num = input(msg)
-
-        if "," in num:
-            num = num.split(",")
-        elif "." in num:
-            num = num.split(".")
+    valido = False
+    while not False:
+        entrada = str(input(msg)).replace(",", ".")
+        if entrada.isalpha() or entrada.strip() == "":
+            print(f'\033[1;31mERRO! "{entrada}" é um preço inválido!\033[0:0m')
         else:
-            num = num.split()
-
-        if len(num) <= 2:
-            cont = 0
-            for c in num:
-                if c.isnumeric():
-                    cont += 1
-            if cont == len(num):
-                num = ".".join(num)
-                return float(num)
-
-        print(f'\033[1;31mERRO! "{num}" é um preço inválido!\033[0:0m')
+            valido = True
+            return float(entrada)
