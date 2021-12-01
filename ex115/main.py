302482e9c634d2ee4314129e7b485a67454839cc
Renan Martins <renanrodm@gmail.com>
2021-12-01 20:26:23 -0300

    ex115b: adicionado sistema de criação e leitura de arquivo

--- ex115/main.py
+++ ex115/main.py
@@ -1,12 +1,18 @@
 from ex115.lib.interface import *
+from ex115.lib.arquivo import *
 from time import sleep
 
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 
 while True:
     resposta = menu(["Ver pessoas cadastradas", "Cadastrar nova Pessoa", "Sair do Sistema"])
     if resposta == 1:
-        cabeçalho("OPÇÃO 1")
+        #opção de lista conteúdo de um arquivo
+        lerArquivo(arq)
     elif resposta == 2:
         cabeçalho("OPÇÃO 2")
     elif resposta == 3:
